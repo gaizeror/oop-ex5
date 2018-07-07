3efee0ea649de2f13e678a564e7781dbbf126dd3
Or Gaizer <gaizeror@gmail.com>
2018-07-07 19:50:33 +0300

    Finished local student

--- students.h
+++ students.h
@@ -3,81 +3,72 @@
 using namespace std;
 
 class Person{
-	string name;
-	int id;
+    string name;
+    int id;
 public:
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
+    Person(string inputName, int inputId): name(inputName), id(inputId) {};
+    Person(Person& p): name(p.name), id(p.id) {};
 
-	virtual void output(ostream& out){
-		out << name << "," << id;
-	}
+    virtual void output(ostream& out){
+        out << name << "," << id;
+    }
 
 };
 
-class Employee : ________________________{
-	string jobTitle;
+class Employee : virtual public Person{
+    string jobTitle;
 public:
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
+    Employee(Person P, string inputJobTitle): Person(P), jobTitle(inputJobTitle){};
+
+    Employee(Employee& e) : Person(e), jobTitle(e.jobTitle){};
+
+    virtual void output(ostream& out) {
+        Person::output(out);
+        out << "," << jobTitle;
+    }
+
+    string getJobTitle() {
+        return jobTitle;
+    }
 };
 
-class Student : ____________________{
-	string major;
-	int year;
+class Student : virtual public Person{
+    string major;
+    int year;
 public:
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
+    Student(Person p, string degreeTitle, int inputYear) : Person(p), major(degreeTitle), year(inputYear) {};
+    Student(Student& s) : Person(s), major(s.major), year(s.year){};
+
+    virtual void output(ostream& out) {
+        Person::output(out);
+        out << "," << major;
+        out << "," << year;
+    }
 };
 
-class Intern : ___________________________{
+class Intern : virtual public Employee, virtual public Student{
 public:
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-		__________________________________________________________________
-
+    Intern(Student& s, Employee& e): Person(s), Student(s), Employee(e) {};
+    virtual void output(ostream& out) {
+        Student::output(out);
+        out << "," << getJobTitle();
+    }
 };
 
 int mainAPIStudent()
 {
-	Person p("israel israeli", 123456789);
+    Person p("israel israeli", 123456789);
 
-	Student s(p, "computer science", 1);
-	s.output(cout);
-	cout << endl; //israel israeli,123456789,computer science,1
+    Student s(p, "computer science", 1);
+    s.output(cout);
+    cout << endl; //israel israeli,123456789,computer science,1
 
-	Employee e(p, "developer");
-	e.output(cout);
-	cout << endl;// israel israeli,123456789,developer
+    Employee e(p, "developer");
+    e.output(cout);
+    cout << endl;// israel israeli,123456789,developer
 
-	Intern i(s, e);
-	i.output(cout);
-	cout << endl; //israel israeli,123456789,computer science,1,develope
-	return 0;
+    Intern i(s, e);
+    i.output(cout);
+    cout << endl; //israel israeli,123456789,computer science,1,develope
+    return 0;
 }
